54479aba265e97deb7c37800e3c88b2b8461f6c4
Jordan Cerasuolo <jcerasuo@trinity.edu>
2026-04-28 10:48:28 -0500

    Initial file upload

---
+++ data.py
@@ -0,0 +1,13 @@
+
+
+from keras.datasets import imdb
+
+training_set, testing_set = imdb.load_data(index_from = 3)
+
+def display_entries(dataset, nums):
+    # **ADD YOUR CODE HERE **
+    
+
+# ** Test code here
+
+
