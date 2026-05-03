401152394605500b5b48bb208d71e0f516349fe0
David-Business <dnilsen@trinity.edu>
2026-05-02 20:09:04 -0500

    Completed Problem 2
    The AI kept wanting me to use X_test and Y_test instead of X_var and Y_Var so if that comes up later, just changing the varible name should be fine

--- lstm.py
+++ lstm.py
@@ -13,11 +13,13 @@ from sklearn.metrics import confusion_matrix
 import seaborn as sns
 
 # Import IMDB dataset keeping only 10,000 most common unique words
-
-
+Dataset = imdb.load_data(num_words=10000)
 # Zero-Padding
 
-
+X_train = sequence.pad_sequences(Dataset[0][0], maxlen=100)
+y_train = Dataset[0][1]
+X_val = sequence.pad_sequences(Dataset[1][0], maxlen=100)
+y_val = Dataset[1][1]
 # Model Building
 
 def train_model(Optimizer, X_train, y_train, X_val, y_val):
